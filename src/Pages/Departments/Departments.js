a9b6edd5b3ec795d1d569ef774576c4f1484d487
https://github.com/AdilMahmoudRion <adilmahmoudrion@gmail.com>
2021-10-20 12:23:57 +0600

    section add

--- src/Pages/Departments/Departments.js
+++ src/Pages/Departments/Departments.js
@@ -1,85 +1,65 @@
-import React from 'react';
-import './Departments.css';
+import React from "react";
+import "./Departments.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faProcedures, faBaby, faHandHoldingMedical, faUserInjured,faBiohazard, faDiagnoses } from "@fortawesome/free-solid-svg-icons";
+import {
+  faProcedures,
+  faBaby,
+  faHandHoldingMedical,
+  faUserInjured,
+  faBiohazard,
+  faDiagnoses,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Departments = () => {
-
-    return (
-      <div id="departments" className="container">
-        <div className="text-center mt-4">
-          <h1 className="fw-bold">Departments</h1>
-          <h3>
-            “Every mountain top is within reach if you just keep climbing.”
-          </h3>
-          <span>— Richard James Molloy</span>
-        </div>
-        <div className="deparments">
-          <div className="dep-section">
-            <div>
-              <h1 className="logo">
-                <FontAwesomeIcon icon={faBaby} />
-              </h1>
-            </div>
-            <div className="dep-title ">
-              <p className="dep-name">Pediatric</p>
-            </div>
-          </div>
-          <div className="dep-section">
-            <div>
-              <h1 className="logo">
-                <FontAwesomeIcon icon={faHandHoldingMedical} />{" "}
-              </h1>
-            </div>
-            <div className="dep-title ">
-              <p className="dep-name">Pulmonary</p>
-            </div>
-          </div>
-          <div className="dep-section">
-            <div>
-              <h1 className="logo">
-                {" "}
-                <FontAwesomeIcon icon={faProcedures} />
-              </h1>
-            </div>
-            <div className="dep-title ">
-              <p className="dep-name">Pulmonary</p>
-            </div>
-          </div>
-          <div className="dep-section">
-            <div>
-              <h1 className="logo">
-                <FontAwesomeIcon icon={faUserInjured} />{" "}
-              </h1>
-            </div>
-            <div className="dep-title ">
-              <p className="dep-name">Cardiology</p>
-            </div>
-          </div>
-          <div className="dep-section">
-            <div>
-              <h1 className="logo">
-                <FontAwesomeIcon icon={faBiohazard} />{" "}
-              </h1>
-            </div>
-            <div className="dep-title ">
-              <p className="dep-name">Urology</p>
-            </div>
-          </div>
-          <div className="dep-section">
-            <div>
-              <h1 className="logo">
-                {" "}
-                <FontAwesomeIcon icon={faDiagnoses} />
-              </h1>
-            </div>
-            <div className="dep-title ">
-              <p className="dep-name">Traumatology</p>
-            </div>
-          </div>
-        </div>
+  return (
+    <div id="departments" className="container">
+      <div className="text-center mt-4">
+        <h1 className="fw-bold">Departments</h1>
+        <h3>“Every mountain top is within reach if you just keep climbing.”</h3>
+        <span>— Richard James Molloy</span>
       </div>
-    );
+
+      <div className="departments-section">
+        <div className="department">
+          <div className="dep-item">
+            <FontAwesomeIcon className="logo" icon={faBaby} />
+            <p className="dep-name mt-2">Pediatric</p>
+          </div>
+        </div>
+        <div className="department">
+          <div className="dep-item">
+            <FontAwesomeIcon className="logo" icon={faHandHoldingMedical} />
+            <p className="dep-name mt-2">Pulmonary</p>
+          </div>
+        </div>
+        <div className="department">
+          <div className="dep-item">
+            <FontAwesomeIcon className="logo" icon={faProcedures} />
+            <p className="dep-name mt-2">Pulmonary</p>
+          </div>
+        </div>
+        <div className="department">
+          <div className="dep-item">
+            <FontAwesomeIcon className="logo" icon={faUserInjured} />
+            <p className="dep-name mt-2">Cardiology</p>
+          </div>
+        </div>
+        <div className="department">
+          <div className="dep-item">
+            <FontAwesomeIcon className="logo" icon={faBiohazard} />
+            <p className="dep-name mt-2">Urology</p>
+          </div>
+        </div>
+        <div className="department">
+          <div className="dep-item">
+            <FontAwesomeIcon className="logo" icon={faDiagnoses} />
+            <p className="dep-name mt-2">Traumatology</p>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  );
 };
 
 export default Departments;
